6e9b83867b8eef52e5b7140bfa9f8cc750224e83
Artyom Pratsko <kabanproff@gmail.com>
2024-07-14 07:28:22 +0300

    edit bannerThird

--- src/components/BannerThird.vue
+++ src/components/BannerThird.vue
@@ -6,51 +6,67 @@ import Button from "./Button.vue";
 <template>
   <div class="banner">
     <div class="container">
-
-    <p>Прохождение теста займет у вас не более
-      <span class="text-primary">12 минут</span>
-      , а его результаты вы сможете <span class="text-primary">использовать всю жизнь.</span>
+    <p>Прохождение теста займет у<br> вас не более
+      <span class="text-primary">12 минут</span>, а его<br> <b>результаты</b> вы сможете <span class="text-primary">использовать всю жизнь.</span>
     </p>
       <div class="field">Профессиональная интерпретация и детально <b>проработанные рекомендации</b> позволят вам качественно <b>изменить все аспекты своей жизни:</b> от финансового до любовного.</div>
     <Button secondary>пойти тест</Button>
     </div>
-
   </div>
-
 </template>
 
 <style scoped>
 .banner {
   position: relative;
+  padding-top: 54px;
+  padding-left: 1px;
+  font-family: var(--merriw-font);
+  font-size: 15px;
+  line-height: 20px;
+  letter-spacing: 0.7px;
+  margin-bottom: 45px;
+}
 
-  &::before,
-  &::after {
-    content: '';
-    display: block;
-    position: absolute;
-  }
+.banner::before,
+.banner::after {
+  content: '';
+  display: block;
+  position: absolute;
+}
 
-  &::before {
-    //z-index: -1;
-    background-image: url("../assets/images/lightning_l.png");
-    //background-position: center;
-  }
+.banner::before {
+  background-image: url("../assets/images/lightning_l.png");
+}
 
-  &::after {
-    //z-index: -1;
-    background-image: url("../assets/images/lightning_r.png");
-  }
+.banner::after {
+  background-image: url("../assets/images/lightning_r.png");
 }
 
 .field {
   background-color: rgba(243, 243, 242, 0.8);
   border-radius: 20px;
-  //padding-top: 20px;
-  //padding-bottom: 20px;
-  padding: 15px 20px;
+  padding: 29px 14px 26px;
+  margin-right: -2px;
+  margin-left: -2px;
   color: var(--text-color-black-2);
+  font-size: 16px;
+  line-height: 23px;
+  letter-spacing: 0.8px;
+  margin-bottom: 33px;
 }
 
+.text-primary {
+  letter-spacing: 1.1px;
+}
+
+p {
+  margin-bottom: 21px;
+}
+
+button {
+  min-width: 199px;
+  padding-bottom: 10px;
+}
 </style>
 
 
